7a2d8c6fc410ce0d476bf19802e5fc8b0090f738
Kerin Khan <kerink585@gmail.com>
2019-01-12 17:08:51 -0800

    need to plan out how to save objects infected

--- client/index.js
+++ client/index.js
@@ -28,16 +28,19 @@ function select_prevention_tool(){
 function give_help(){
   /*this function should give the selected preventive meth to a character.*/
 }
-function Character (x,y,width,height,life,infected,outside,sleeping){
+function setTimerDeath(val){
+  val = 0;
+  context.fillStyle = "black";
+  context.fill();
+}
+function Character (x,y,radius,isHuman, isInsect, isAnimal){
   // character object
   this.x = x;
   this.y = y;
-  this.width = width;
-  this.height = height;
-  this.life = life;
-  this.infected = infected;
-  this.outside = outside;
-  this.sleeping = sleeping;
+  this.radius = radius;
+  this.isHuman = isHuman;
+  this.isInsect = isInsect;
+  this.isAnimal = isHuman;
   this.good = colors[0];
   this.sick = colors[1];
   this.infected = INFECTED[Math.floor(Math.random()*INFECTED.length)];
@@ -45,27 +48,34 @@ function Character (x,y,width,height,life,infected,outside,sleeping){
   //instance methods of class
   this.draw = function(){
     context.beginPath();
-    context.arc(this.x, this.y, 30, 0, 2 * Math.PI);
-    if(this.infected){
+    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    if(this.isHuman){
+      if(this.infected){
+        context.fillStyle = this.sick;
+   		 context.fill();
+       }
+       else{
+         context.fillStyle = this.good;
+    		 context.fill();
+       }
 
-		 context.fillStyle = this.sick;
-		 context.fill();
-    }
-    else{
-      context.fillStyle = this.good;
- 		 context.fill();
-
-    }
+      }
+      if(this.isInsect){
+        context.fillStyle = "black";
+        context.fill();
+      }
     }
 
   this.update = function(){
     this.draw()
   }
 }
+var insect =[];
 
 var human = [];
 for (var i=0; i<5; i++){
-  human.push(new Character(Math.random()*canvas.width,Math.random()*canvas.height,10,10));
+  human.push(new Character(Math.random()*canvas.width-30*2,Math.random()*canvas.height-30,30,true,false,false));
+  insect.push(new Character(Math.random()*canvas.width-5*2,Math.random()*canvas.height-5,5,false,true,false));
 }
 console.log(human);
 
@@ -75,6 +85,7 @@ function animate(){
 
   for (var i =0;i<human.length;i++){
     human[i].update();
+    insect[i].update();
   }
 }
 animate();
