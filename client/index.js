56ed3ec4b00a8abd519f79e9f368ba39e881536b
Kerin Khan <kerink585@gmail.com>
2019-01-23 16:43:40 -0800

    deleted previous extra functions

--- client/index.js
+++ client/index.js
@@ -13,21 +13,7 @@ const colors= ['green','purple'];
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// character of the game
-// character will be stored in a object
-function menu(){
-  /*this function will show the main menu. the player will be able to choose to
-  play the simulation/game  get game instructions and statistics of game.*/
-}
-function save_game(){
-  /*this function will save the current state of the game*/
-}
-function select_prevention_tool(){
-  /*The player will be able to select a tool to help characters from disease.*/
-}
-function give_help(){
-  /*this function should give the selected preventive meth to a character.*/
-}
+
 function setTimerDeath(val){
   val = 0;
   context.fillStyle = "black";
